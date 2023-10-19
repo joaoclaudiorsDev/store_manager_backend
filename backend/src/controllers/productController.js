e59a5f497e78cfbb53908dd944094cb87a812dcc
João Cláudio <joaoclaudiobio@gmail.com>
2023-10-19 09:52:59 -0300

    productController

---
+++ backend/src/controllers/productController.js
@@ -0,0 +1,39 @@
+const productService = require('../services/productService');
+const status = require('../../utils/status'); 
+
+async function listAllProducts(req, res) {
+  try {
+    const products = await productService.getAllProducts();
+    res.status(status.httpStatus('OK')).json(products);
+  } catch (error) {
+    res.status(status.httpStatus('INTERNAL_SERVER_ERROR'))
+      .json({ error: status.ErrorMessages.INTERNAL_SERVER_ERROR });
+  }
+}
+  
+async function getProductById(req, res) {
+  const productId = req.params.id;
+  if (!productId) {
+    return res.status(status.httpStatus('BAD_REQUEST'))
+      .json({ error: status.ErrorMessages.BAD_REQUEST });
+  }
+  
+  try {
+    const product = await productService.getProductById(productId);
+  
+    if (!product) {
+      return res.status(status.httpStatus('NOT_FOUND'))
+        .json({ error: status.ErrorMessages.NOT_FOUND });
+    }
+  
+    res.status(status.httpStatus('OK')).json(product);
+  } catch (error) {
+    res.status(status.httpStatus('INTERNAL_SERVER_ERROR'))
+      .json({ error: status.ErrorMessages.INTERNAL_SERVER_ERROR });
+  }
+}
+  
+module.exports = {
+  listAllProducts,
+  getProductById,
+};
