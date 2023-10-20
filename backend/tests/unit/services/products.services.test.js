9117403376dfd3cbf0f61493f526c037950a1308
João Cláudio <joaoclaudiobio@gmail.com>
2023-10-20 10:08:36 -0300

    test productService

--- backend/tests/unit/services/products.services.test.js
+++ backend/tests/unit/services/products.services.test.js
@@ -2,18 +2,31 @@ const { expect } = require('chai');
 const sinon = require('sinon');
 
 const productList = require('../mocks/product');
-const Services = require('../../../src/services');
+const { productService } = require('../../../src/services');
+const { productModel } = require('../../../src/models');
 
 describe('Services', function () {
-  afterEach(function () { return sinon.restore(); });
-
-  it('', async function () {
-    sinon.stub(Services, 'getAllProducts').resolves(productList);
-    const allProducts = await Services.getAllProducts();
+  afterEach(function () {
+    sinon.restore();
   });
 
-  it('', async function () {
-    sinon.stub(Services, 'getProductById').resolves(productList[0]);
-    const product = await Services.getProductById(1);
+  it('should return all products', async function () {
+    sinon.stub(productModel, 'getAllProducts').resolves(productList);
+
+    const result = await productService.getAllProducts();
+
+    expect(result.status).to.equal('OK');
+    expect(result.data).to.deep.equal(productList);
+  });
+
+  it('should return a product by ID', async function () {
+    const id = 1;
+
+    sinon.stub(productModel, 'getProductById').withArgs(id).resolves(productList[0]); // Use productList
+
+    const result = await productService.getProductById(id);
+
+    expect(result.status).to.equal('OK');
+    expect(result.data).to.deep.equal(productList[0]);
   });
 });
